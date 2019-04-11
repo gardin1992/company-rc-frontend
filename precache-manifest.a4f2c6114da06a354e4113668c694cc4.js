3d3a94670e3ff3dfec6227194a04c1ba56d841a2
Joao Gardin <gardin1992@gmail.com>
2019-04-11 01:24:26 -0300

    Updates

---
+++ precache-manifest.a4f2c6114da06a354e4113668c694cc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/control_budgets/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58546e37eb624c14354b",
+    "url": "/control_budgets/static/js/runtime~main.74f82dfc.js"
+  },
+  {
+    "revision": "5872d286532550e84d89",
+    "url": "/control_budgets/static/js/main.b129cdaf.chunk.js"
+  },
+  {
+    "revision": "841b373786906e5d3af4",
+    "url": "/control_budgets/static/js/2.65009ed0.chunk.js"
+  },
+  {
+    "revision": "5872d286532550e84d89",
+    "url": "/control_budgets/static/css/main.0acd3a90.chunk.css"
+  },
+  {
+    "revision": "841b373786906e5d3af4",
+    "url": "/control_budgets/static/css/2.7618634a.chunk.css"
+  },
+  {
+    "revision": "95ed3c7ae9ef83114f3fa45ec94c1894",
+    "url": "/control_budgets/index.html"
+  }
+];
